3033d82376a8969e2a19b0e939a6c26c6868e78a
lambert <dementjew.vania2016@yandex.ru>
2024-11-15 01:42:33 +0300

    15.11.2024, 1:42, lambert

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
+
 def index(request):
     return render(request, 'main/index.html')
+
 
 def about(request):
     return render(request, 'main/about.html')
 
+
 def news(request):
-    return render(request, 'main/news.html')
+    news = News.objects.order_by('-date')
+    return render(request, 'main/news.html', {'news': news})

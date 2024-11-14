0b6e93fc2e7ca6fac40c5a01c1689816a14fceea
lambert <dementjew.vania2016@yandex.ru>
2024-11-15 00:54:15 +0300

    15.11.2024, 0:53, lambert

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class News(models.Model):
+    id = models.IntegerField(primary_key=True)
+    date = models.DateTimeField()
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.title
 # Create your models here.
